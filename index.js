77e98a4ebce9bbcdbfa6146394787533d1df6fe7
sondos alansi <sondos.alansi@yahoo.com>
2018-09-04 08:34:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const drivers=["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name){
+  drivers.pop(name);
+}
+function destructivelyRemoveFirstDriver(name){
+  drivers.shift(name);
+}
+function appendDriver(name){
+ 
+}
+function 
+
+
+function removeLastDriver(){
+  drivers.splice(-1);
+}
+function removeFirstDriver(){
+  drivers.splice(0);
+}
